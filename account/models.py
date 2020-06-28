c82a1c7c950ca4aa79f846a1087d1be95aaa4115
Fshakiba <farhadshakiba@yahoo.com>
2020-06-28 15:45:57 +0430

    initial

--- account/models.py
+++ account/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.core import signing
+from django.core.signing import SignatureExpired, BadSignature
+from allauth.account.models import EmailAddress
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type = models.CharField(
+        choices=(('Candidate', 'Candidate'), ('Recruiter', 'Recruiter'), ('Employer', 'Employer'),),
+        max_length=50,
+    )
+    last_modified = models.DateTimeField(auto_now_add=False, auto_now=True)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self):
+        return self.user.email
+
+    def generate_token(self):
+        email = self.user.email
+        token = signing.dumps({'email': email})
+        return token
+
+    @staticmethod
+    def verify_token(token, max_age=604800):
+        # default max_age is 7 days
+        try:
+            value = signing.loads(token, max_age=max_age)
+        except SignatureExpired:
+            return None
+        except BadSignature:
+            return None
+        user = User.objects.get(email=value['email'])
+        return user
+
+
+def create_account_emailaddress(sender, instance, created, **kwargs):
+    # Used for django-allauth
+    if created:
+        EmailAddress.objects.get_or_create(
+            user_id=instance.id,
+            email=instance.email
+        )
+
+
+post_save.connect(create_account_emailaddress, sender=User)
---
+++ interview/views.py
@@ -0,0 +1,74 @@
+import json
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from interview.models import Available, InterviewRequest
+
+
+def available(request, bu_id):
+    user = User.objects.get(id=bu_id)
+    context = {'user': user}
+    return render(request, 'interviews/available.html', context)
+
+
+def availability(request, bu_id):
+    user = User.objects.get(id=bu_id)
+    if request.method == 'GET':
+        user_availabiity = user.available_set.all()
+        availability = []
+        for avail in user_availabiity:
+            temp = {
+                "day": str(avail.day_of_week),
+                "start": avail.time_start,
+                "end": avail.time_end
+            }
+            availability.append(temp)
+        availability = json.dumps(availability)
+        return JsonResponse({'availability': availability})
+
+    if request.method == 'POST':
+        old_availability = user.available_set.all()
+        new_availability = json.loads(request.POST.get('availability'))
+        timezone = json.loads(request.POST.get('timezone'))
+        available_instances = []
+        for time_range in new_availability:
+            avail = Available(
+                day_of_week=int(time_range['day']),
+                time_start=time_range['start'],
+                time_end=time_range['end'],
+                user=user
+            )
+            available_instances.append(avail)
+        old_availability.delete()
+        Available.objects.bulk_create(available_instances)
+        message = {'message': 'Availability updated'}
+        if timezone != user.userprofile.timezone:
+            user.userprofile.timezone = timezone
+            user.userprofile.save()
+            message['message'] = 'Timezone and ' + message['message']
+        return JsonResponse(message)
+
+
+@login_required
+def interview_requests(request):
+    user = request.user
+    user_type = user.userprofile.user_type
+    if user_type == 'Candidate':
+        interview_requests = InterviewRequest.objects.filter(
+            candidate=user.candidate).all()
+    elif user_type == 'Recruiter':
+        interview_requests = InterviewRequest.objects.filter(
+            job__recruiter=user.recruiter).all()
+    elif user_type == 'Employer':
+        interview_requests = InterviewRequest.objects.filter(
+            job__employer=user.employer).all()
+    elif user.is_staff:
+        interview_requests = InterviewRequest.objects.all()
+    else:
+        raise PermissionDenied
+
+    return render(request,
+                  'interviews/interviews.html',
+                  {'interview_requests': interview_requests})
